f91173b90ac8f1bd88cdc56465e49557d5e6866d
Byungkuk Choi <litlpoet@gmail.com>
2018-08-01 01:24:04 +1200

    add map test with sparse mat

--- test/unittests/test_core_sparsemat.cpp
+++ test/unittests/test_core_sparsemat.cpp
@@ -82,3 +82,40 @@ TEST_CASE("Sparse matrix IO", "[CoreSparseMatrix]")
   std::cout << "Dense mat (output):\n" << MatB << std::endl;
   REQUIRE(MatA.cwiseEqual(MatB).count() == (4 * 5));
 }
+
+TEST_CASE("SparseMat Map", "[CoreSparseMatrix]")
+{
+  std::vector<eigenutils::core::SpTrp> triplets;
+  triplets.emplace_back(eigenutils::core::SpTrp(0, 0, 1));
+  triplets.emplace_back(eigenutils::core::SpTrp(0, 2, 3));
+  triplets.emplace_back(eigenutils::core::SpTrp(0, 3, 4));
+  triplets.emplace_back(eigenutils::core::SpTrp(1, 3, 2));
+  triplets.emplace_back(eigenutils::core::SpTrp(2, 2, -1));
+  triplets.emplace_back(eigenutils::core::SpTrp(2, 3, 3));
+  triplets.emplace_back(eigenutils::core::SpTrp(3, 0, -2));
+  triplets.emplace_back(eigenutils::core::SpTrp(3, 4, 7));
+
+  eigenutils::core::SpMatRM sparse_mat(4, 5);
+  sparse_mat.setFromTriplets(triplets.cbegin(), triplets.cend());
+
+  std::vector<double>                data(4 * 5);
+  Eigen::Map<eigenutils::core::MatX> data_mat(data.data(), 4, 5);
+
+  std::vector<double>                  data2(4 * 5);
+  Eigen::Map<eigenutils::core::MatXRM> data2_mat(data2.data(), 4, 5);
+
+  data_mat  = sparse_mat;
+  data2_mat = sparse_mat;
+
+  std::cout << "sparse_mat: " << std::endl << sparse_mat << std::endl;
+  std::cout << "data_mat : " << std::endl << data_mat << std::endl;
+  std::cout << "data_mat2: " << std::endl << data2_mat << std::endl;
+  std::cout << "vector list: ";
+  for (auto const& d : data)
+    std::cout << d << ", ";
+  std::cout << std::endl;
+  std::cout << "vector list2: ";
+  for (auto const& d : data2)
+    std::cout << d << ", ";
+  std::cout << std::endl;
+}
